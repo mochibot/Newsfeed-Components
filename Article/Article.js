4f420dad11a6a093134bdebbd8b47ba6ddd4e1f2
mochibot <amusaur@gmail.com>
2019-05-15 13:07:39 -0700

    WIP: articles completed

--- Article/Article.js
+++ Article/Article.js
@@ -3,18 +3,26 @@
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    this.expandButton.textContent = 'expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', () => {
+      this.expandArticle();
+      this.expandButton.textContent === 'expand' ? this.expandButton.textContent = 'collapse' : this.expandButton.textContent = 'expand';
+    })
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
+    /*
+    While I could just toggle the .article-open class, the css limits the height to 400px and the longer text gets cut off, so I changed the height to 'auto'
 
+    this.domElement.classList.toggle('article-open');
+    */
+    this.domElement.style.height === 'auto' ? this.domElement.style.height = '50px' : this.domElement.style.height = 'auto';
   }
 }
 
@@ -26,4 +34,8 @@ class Article {
 
 */
 
-let articles;
+let articles = document.querySelectorAll('.article');
+
+articles.forEach(element => {
+  const articleInstance = new Article(element);
+})
